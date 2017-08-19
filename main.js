e33a6991ddbda7f309e43b674d2fe77c6578f2a4
Anson <aponty@gmail.com>
2017-08-19 13:30:00 -0400

    menu plugin added

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 "use strict";
-window.onload = () => {
-    console.log('linked')
-    const scroll = skrollr.init({
-        constants: {
-            //fill the box for a "duration" of 150% of the viewport (pause for 150%)
-            //adjust for shorter/longer pause
-            box: '100p'
-        },
-        //for debugging
-        render: function(data) {
-            console.log(data.curTop)
-        }
-    });
-}
+
+const scroll = skrollr.init({
+    constants: {
+        //fill the box for a "duration" of 150% of the viewport (pause for 150%)
+        //adjust for shorter/longer pause
+        box: '100p'
+    },
+    //for debugging
+    render: function(data) {
+        console.log(data.curTop)
+    }
+});
+
+skrollr.menu.init(scroll)
