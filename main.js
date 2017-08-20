0e8e2baf3b71fef3832399e8c152a65788e1dc60
Anson <aponty@gmail.com>
2017-08-19 20:00:13 -0400

    styling cleanup, nav working, ready for more content

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 "use strict";
+window.onload = () => {
+    //prevents skrollr from jumping to leftover hash in url on refresh
+    document.location.hash = null;
 
-const scroll = skrollr.init({
-    constants: {
-        //fill the box for a "duration" of 150% of the viewport (pause for 150%)
-        //adjust for shorter/longer pause
-        box: '100p'
-    },
-    //for debugging
-    render: function(data) {
-        console.log(data.curTop)
-    }
-});
+    const scroll = skrollr.init({
+        constants: {
+            box: '100p'
+        },
+        //for debugging
+        render: function(data) {
+            console.log(data.curTop)
+        }
+    });
 
-skrollr.menu.init(scroll)
+    skrollr.menu.init(scroll)
+
+
+
+}
